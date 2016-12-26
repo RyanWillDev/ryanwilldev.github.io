7c1dd6ef4384532159d7558449fcea82de898bc7
Ryan Will <ryanwilldev@gmail.com>
2016-12-26 15:50:17 -0600

    Renames JournalEntries to Journal, adds logic to display journal entries

---
+++ src/components/Journal.vue
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+  import journal from '../journal/journal.json';
+
+  export default {
+    name: 'Journal',
+    data() {
+      return {
+        journal,
+      };
+    },
+  };
+</script>
+
+<template>
+  <section>
+    <h1 class="section-head">Journal</h1>
+    <ul>
+      <li v-for="entry in journal">
+        <router-link :to="entry.data.url" append><h3>{{entry.data.title}}</h3></router-link>
+        <p>{{entry.data.description}}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style type="text/css"></style>
---
+++ src/components/JournalEntry.vue
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+/*eslint-disable*/
+  import hljs from 'highlight.js';
+
+  import journal from '../journal/journal.json';
+
+  function highlightCode() {
+    const code = document.querySelectorAll('code');
+    console.log(code);
+    code.forEach((block) => {
+      console.log(block);
+      hljs.highlightBlock(block);
+    });
+  }
+
+  export default {
+    name: 'JournalEntry',
+    data() {
+      return {
+        entry: journal.find(curr => curr.data.url === this.$route.params.entryUrl),
+      };
+    },
+    mounted() {
+      highlightCode();
+    }
+  };
+</script>
+
+<template>
+<div>
+  <h1>{{ entry.data.title }}</h1>
+  <div v-html="entry.html"></div>
+</div>
+</template>
+
+<style type="text/css">
+  
+</style>
