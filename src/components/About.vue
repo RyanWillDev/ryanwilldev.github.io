5428728e6f59c96a743541aa8dc6d9bf263ee25f
Ryan Will <ryanwilldev@gmail.com>
2016-12-31 18:10:56 -0600

    Rewords p, changes some styling

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,8 +7,8 @@
 <template>
   <div class="about-container">
     <h1 class="section-head">Who is Ryan Will?</h1>
-    <p>I am a passionate and motivated self taught web developer from Middle Tennessee. I currently work at <a href="https://metova.com">Metova</a> and specialize in making rich and interactive web applications using modern techniques and technologies. </p>
-    <p>I am proficient with JavaScript, and have experience with popular frameworks and libraries such as Node, React, Vue, and Angular. I am currently working on expanding my knowledge of computer science in general to become a better software engineer. If you'd like to know more, just <a target="_blank" href="https://twitter.com/intent/tweet/?screen_name=RyanWillDev">ask</a>!</p>
+    <p>I am a passionate and motivated self taught web developer from Middle Tennessee. I currently work at <a target="_blank" href="https://metova.com">Metova</a> and specialize in making rich and interactive web applications using modern techniques and technologies. </p>
+    <p>I am proficient with JavaScript, and have experience with popular frameworks and libraries such as Node, React, Redux, Vue, and Angular. I am currently working on expanding my knowledge of computer science in general to become a better software engineer. If you'd like to know more, just <a target="_blank" href="https://twitter.com/intent/tweet/?screen_name=RyanWillDev">ask</a>!</p>
   </div>
 </template>
 
@@ -26,11 +26,11 @@
 
   @media screen and (min-width: 1000px) {
     .about-container {
-      padding: 0 30px 0;
+      padding: 0;
     }
 
     p {
-      padding: 10px 20px 0;
+      padding: 10px 0;
     }
   }
 </style>
