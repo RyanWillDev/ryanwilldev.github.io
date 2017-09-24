b0d1d9ca2d3ad993e74daeb7f64894f7dbcf754b
Ryan Will <ryanwilldev@gmail.com>
2017-09-24 12:56:43 -0500

    adds meta information

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,27 @@ module.exports = {
   router: {
     linkActiveClass: 'router-link-active',
     linkExactActiveClass: 'router-link-active',
-    middleware: 'redirectAtRoot'
-  }
+    middleware: 'redirectAtRoot',
+  },
+  head: {
+    title: 'RyanWillDev | Software Developer',
+    link: [
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/atom-one-dark.min.css' },
+      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
+      { rel: 'canonical', href: 'https://ryanwilldev.com' },
+    ],
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: 'utf8' },
+      { name: 'description', content: 'Web Developer in the Nashville area with a passion for learning and creating custom websites and applications.', hid: 'desc' },
+      { name: 'title', content: 'RyanWillDev' },
+      { property: 'og:description', content: 'Web Developer in the Nashville area with a passion for learning and creating custom websites and applications.', hid: 'og:desc' },
+      { property: 'og:title', content: 'RyanWillDev', hid: 'og:title' },
+      { property: 'og:url', content: 'https://ryanwilldev.com', hid: 'og:url' },
+      { property: 'twitter:description', content: 'Web Developer in the Nashville area with a passion for learning and creating custom websites and applications.', hid: 't:desc' },
+      { property: 'twitter:domain', content: 'ryanwilldev.com', hid: 't:domain' },
+      { property: 'twitter:tile', content: 'RyanWillDev', hid: 't:tile' },
+    ],
+  },
 };
