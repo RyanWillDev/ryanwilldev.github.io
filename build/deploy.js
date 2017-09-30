16d16faf08ecd0dace7bdd13840b056e2010abdd
Ryan Will <ryanwilldev@gmail.com>
2017-09-30 13:19:11 -0500

    fixes deploy script

--- build/deploy.js
+++ build/deploy.js
@@ -1,9 +1,9 @@
 const util = require('util'),
       exec = require('child_process').exec,
-      commitMessage = 'echo ' + process.argv.slice(2);
+      commitMessage = process.argv.slice(2)[0];
 
 const childProcess =
-  exec('git add dist/ && git commit -m ' + commitMessage + ' && git subtree push --prefix dist/ origin master',
+  exec('git add dist/ && git commit -m "' + commitMessage + '" && git subtree push --prefix dist/ origin master',
     (err, stdout, stderr) => {
       process.stdout.write(stdout);
       process.stderr.write(stderr);
