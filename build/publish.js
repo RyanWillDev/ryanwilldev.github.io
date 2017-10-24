11c923d67b3d7b7037f6b8e75334a50127ad7656
Ryan Will <ryanwilldev@gmail.com>
2017-10-24 13:11:48 -0500

    Sorts entries by publicationDate
    Formats publicationDate after sorting since the date string is now
    stored in the metadata JSON for each post

--- build/publish.js
+++ build/publish.js
@@ -12,15 +12,39 @@ const journal = entries.reduce((prev, dir) => {
   const entry = {};
 
   fs.readdirSync(entriesPath + dir)
-  .forEach((file) => {
-    if (file.includes('json')) {
-      const data = JSON.parse(fs.readFileSync(`${entriesPath}${dir}/${file}`, 'utf8'));
-      entry.data = data;
-    } else {
-      const html = converter.makeHtml(fs.readFileSync(`${entriesPath}${dir}/${file}`, 'utf8'));
-      entry.html = html;
-    }
-  });
-  return prev.concat(entry);
-}, []);
+    .forEach((file) => {
+      if (file.includes('json')) {
+        const data = JSON.parse(fs.readFileSync(`${entriesPath}${dir}/${file}`, 'utf8'));
+        entry.data = data;
+      } else {
+        const html = converter.makeHtml(fs.readFileSync(`${entriesPath}${dir}/${file}`, 'utf8'));
+        entry.html = html;
+      }
+    });
+    return prev.concat(entry);
+}, [])
+.sort((p, n) => {
+  const pDate = Date.parse(p.data.publicationDate),
+        nDate = Date.parse(n.data.publicationDate);
+  console.log(pDate > nDate);
+
+  if (pDate > nDate) return -1;
+  else if (pDate < nDate) return 1;
+  else return 0;
+})
+.map(entry => {
+  entry.data.publicationDate = formatDate(entry.data.publicationDate);
+  return entry;
+});
+
 fs.writeFileSync(`${path}journal.json`, JSON.stringify(journal));
+
+function formatDate(date) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        pubDate = new Date(date),
+        pubMonth = pubDate.getMonth(),
+        pubDay = pubDate.getDate(),
+        pubYear = pubDate.getFullYear();
+
+  return `${months[pubMonth]} ${pubDay}, ${pubYear}`;
+}
