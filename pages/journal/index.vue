4ab688aa6d0a6154192b7edcbb9a2e2c0d2c2c79
Ryan Will <ryanwilldev@gmail.com>
2018-01-07 12:44:02 -0600

    Uses query params to perserve the selected tag

--- pages/journal/index.vue
+++ pages/journal/index.vue
@@ -1,40 +1,45 @@
 <script type="text/javascript">
-  import journal from '../../journal/journal.json';
+import journal from '../../journal/journal.json';
 
-  export default {
-    name: 'Journal',
-    data() {
-      return {
-        selectedTag: '',
-      };
+export default {
+  name: 'Journal',
+  props: {
+    journal: Array,
+  },
+  methods: {
+    capitalize(tag) {
+      return tag
+        .split(' ')
+        .map(word => word[0].toUpperCase().concat(word.slice(1)))
+        .join(' ');
     },
-    props: {
-      journal: Array,
+
+    filteredEntries() {
+      if (!this.$route.query.tag) return journal;
+
+      return journal.filter(entry =>
+        entry.data.tags.includes(this.$route.query.tag),
+      );
     },
-    methods: {
-      capitalize(tag) {
-        return tag.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
-      },
-
-      updateSelectedTag(tag) {
-        this.selectedTag = tag;
-      },
-
-      filteredEntries() {
-        if (this.selectedTag.length) {
-          return journal.filter(entry => entry.data.tags.includes(this.selectedTag));
-        }
-
-        return journal;
-      },
+  },
+  computed: {
+    tagSelected() {
+      return !!this.$route.query.tag;
     },
-  };
+  },
+};
 </script>
 
 <template>
   <section>
     <h1 class="section-head">Journal</h1>
-    <span class="clear-btn" v-show="selectedTag" @click="updateSelectedTag('')">See All Entries</span>
+    <nuxt-link
+      replace
+      class="clear-btn"
+      v-show="tagSelected"
+      :to="'/journal'">
+      See All Entries
+    </nuxt-link>
     <ul>
       <li v-for="(entry, index) in filteredEntries()" :key="index">
         <nuxt-link :to="entry.data.url" append>
@@ -43,7 +48,13 @@
         </nuxt-link>
         <p>{{entry.data.description}}</p>
         <div class="tags">
-          <span class="tag" v-for="(tag, i) in entry.data.tags" :key="i" @click="updateSelectedTag(tag)">{{capitalize(tag)}}</span>
+          <nuxt-link
+            class="tag"
+            :to="{ query: { tag: tag } }"
+            v-for="(tag, i) in entry.data.tags"
+            :key="i">
+              {{capitalize(tag)}}
+          </nuxt-link>
         </div>
       </li>
     </ul>
@@ -51,77 +62,79 @@
 </template>
 
 <style lang="scss" scoped>
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
+@import '../../scss/variables';
 
-    li {
-      margin: 32px 0;
-    }
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin: 32px 0;
   }
+}
 
-  h3 {
-    color: #313233;
-    display: inline-block;
-    margin: 0 12px 0 0;
-  }
+h3 {
+  color: $offBlack;
+  display: inline-block;
+  margin: 0 12px 0 0;
+}
 
-  a {
-    color: #313233;
-    text-decoration: none;
+a {
+  color: $offBlack;
+  text-decoration: none;
 
-    &:hover {
-      & h3 {
-        text-decoration: underline;
-      }
-    }
-  }
-
-  p {
-    margin-top: 8px;
-  }
-
-  .tags {
-
-    .tag {
-      height: 100%;
-    }
-
-    @media screen and (max-width: 500px) {
-      display: flex;
-      flex-wrap: wrap;
-
-      .tag {
-        margin-top: 8px;
-      }
-    }
-  }
-
-  .clear-btn {
-    width: 15%;
-    align-self: flex-end;
-    text-align: center;
-  }
-
-  .clear-btn, .tag {
-    background-color: #bcbcbc;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: .8em;
-    font-weight: bold;
-    margin-right: 8px;
-    padding: 5px;
-
-    &:hover {
-      padding: 8px;
+  &:hover {
+    & h3 {
       text-decoration: underline;
     }
   }
+}
 
-  .pub-date {
-    color: lighten(#313233, 30%);
-    font-size: .85em;
-    margin-top: 8px;
+p {
+  margin-top: 8px;
+}
+
+.tags {
+  .tag {
+    height: 100%;
   }
+
+  @media screen and (max-width: 500px) {
+    display: flex;
+    flex-wrap: wrap;
+
+    .tag {
+      margin-top: 8px;
+    }
+  }
+}
+
+.clear-btn {
+  width: 15%;
+  align-self: flex-end;
+  text-align: center;
+}
+
+.clear-btn,
+.tag {
+  background-color: $lightGray;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 0.8em;
+  font-weight: bold;
+  margin-right: 8px;
+  padding: 5px;
+
+  &:hover {
+    padding: 8px;
+    text-decoration: underline;
+  }
+}
+
+.pub-date {
+  color: lighten($offBlack, 30%);
+  font-size: 0.85em;
+  margin-top: 8px;
+}
 </style>
