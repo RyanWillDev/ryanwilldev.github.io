2e5597d6633ca3717c177f68696df338d32c2f93
Ryan Will <ryanwilldev@gmail.com>
2018-01-07 11:54:42 -0600

    Rewords about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -7,19 +7,36 @@ export default {
 <template>
   <div>
     <h1>Who is Ryan Will?</h1>
-    <p>I am a passionate and motivated self taught software developer from Middle Tennessee. I currently work at <a target="_blank" href="https://metova.com">Metova</a>
-      and specialize in making rich and interactive web applications using modern techniques and technologies.
+    <p>
+      A passionate and motivated, self-taught web developer from the Nasville Tennessee area.
+      I am proficient with JavaScript and have experience with popular frameworks and libraries such as Node, React, Redux, Vue, and Angular.
     </p>
 
-    <p>I am proficient with JavaScript, and have experience with popular frameworks and libraries such as Node, React, Redux, Vue, and Angular.
-      I am currently working on expanding my knowledge of computer science in general to become a better software engineer.
+    <p>
+      Currently, I create rich, interactive web applications using modern techniques and technologies at <a target="_blank" href="https://metova.com">Metova</a>.
+    </p>
+
+    <p>
+      I am always striving to become a better software developer by learning new technologies, techniques, and languages.
+      My current focus is on Progressive Web Apps.
+    </p>
+
+    <p>
       You can check out some of my work <a href="https://github.com/ryanwilldev">here</a>.
       If you'd like to know more, just <a target="_blank" href="https://twitter.com/intent/tweet/?screen_name=RyanWillDev">ask</a>!
     </p>
   </div>
 </template>
 
-<style type="text/css" scoped>
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 32px;
+
+  @media screen and (min-width: 1100px) {
+    margin-bottom: 64px;
+  }
+}
+
 p {
   font-size: 1.15em;
 }
