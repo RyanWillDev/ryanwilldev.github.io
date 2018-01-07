20644438006a279aac910dcd2bbc212f4c0b6b90
Ryan Will <ryanwilldev@gmail.com>
2018-01-06 18:04:51 -0600

    Adds component to centralize the formatting for each page

---
+++ components/FormattedPage.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: 'FormattedPage',
+};
+</script>
+
+<template>
+  <section class="formatted-page">
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="scss">
+@import '../scss/variables';
+
+.formatted-page {
+  color: $offBlack;
+  font-size: 1.1em;
+  line-height: 1.25;
+  margin-top: 2.5%;
+  text-align: left;
+
+  a {
+    color: $blueAccent;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+
+  @media screen and (min-width: 1100px) {
+    margin: 48px 3% 0 calc(#{$maxNavbarWidthL} + 2%);
+    width: 70%;
+  }
+}
+</style>
+
+
+
+
+
