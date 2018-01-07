f2e2ff5128a0525c7cb20257997182e7d6e0700d
Ryan Will <ryanwilldev@gmail.com>
2018-01-07 10:23:51 -0600

    Removes left padding to format journal entries like the rest of the pages

--- pages/journal/_journalEntry.vue
+++ pages/journal/_journalEntry.vue
@@ -5,7 +5,7 @@ import journal from '../../journal/journal.json';
 
 function highlightCode() {
   const code = document.querySelectorAll('code'); // eslint-disable-line
-  code.forEach((block) => {
+  code.forEach(block => {
     hljs.highlightBlock(block);
   });
 }
@@ -14,7 +14,9 @@ export default {
   name: 'JournalEntry',
   async asyncData({ payload, params }) {
     return {
-      entry: payload || journal.find(entry => entry.data.url === params.journalEntry),
+      entry:
+        payload ||
+        journal.find(entry => entry.data.url === params.journalEntry),
     };
   },
   mounted() {
@@ -23,15 +25,39 @@ export default {
   head() {
     return {
       link: [
-        { rel: 'canonical', href: `https://ryanwilldev.com/journal/${this.entry.data.url}`, hid: 'con' },
+        {
+          rel: 'canonical',
+          href: `https://ryanwilldev.com/journal/${this.entry.data.url}`,
+          hid: 'con',
+        },
       ],
       meta: [
-        { name: 'description', content: this.entry.data.description, hid: 'desc' },
-        { property: 'og:title', content: this.entry.data.title, hid: 'og:title' },
-        { property: 'og:description', content: this.entry.data.description, hid: 'og:desc' },
-        { property: 'og:url', content: `https://ryanwilldev.com/journal/${this.entry.data.url}`, hid: 'og:url' },
+        {
+          name: 'description',
+          content: this.entry.data.description,
+          hid: 'desc',
+        },
+        {
+          property: 'og:title',
+          content: this.entry.data.title,
+          hid: 'og:title',
+        },
+        {
+          property: 'og:description',
+          content: this.entry.data.description,
+          hid: 'og:desc',
+        },
+        {
+          property: 'og:url',
+          content: `https://ryanwilldev.com/journal/${this.entry.data.url}`,
+          hid: 'og:url',
+        },
         { property: 'og:type', content: 'article', hid: 'og:type' },
-        { name: 'twitter:description', content: this.entry.data.description, hid: 't:desc' },
+        {
+          name: 'twitter:description',
+          content: this.entry.data.description,
+          hid: 't:desc',
+        },
       ],
     };
   },
@@ -48,55 +74,54 @@ export default {
 </template>
 
 <style lang="scss">
-  .entry-container {
-    h1 {
-      margin-bottom: .20em;
+.entry-container {
+  h1 {
+    margin-bottom: 0.2em;
+  }
+
+  h2,
+  h3 {
+    margin-bottom: 24px;
+    margin-top: 56px;
+  }
+
+  pre {
+    white-space: pre-wrap;
+
+    @media screen and (max-width: 750px) {
+      font-size: 0.8em;
     }
 
-    h2, h3 {
-      margin-bottom: 24px;
-      margin-top: 56px;
-    }
-
-    pre {
-      white-space: pre-wrap;
-
-      @media screen and (max-width: 750px) {
-        font-size: .8em;
-      }
-
-      code {
+    code {
       border-radius: 5px;
       margin: 24px 0;
       width: 90%;
-      }
-    }
-
-    .pub-date {
-      display: block;
-      margin-bottom: 24px;
-
-      @media screen and (min-width: 750px) {
-        margin-bottom: 48px;
-      }
-    }
-
-    .hljs-keyword {
-      border-radius: 5px;
-      display: inline-flex;
-      padding: 2px 4px;
-    }
-
-    p > code.hljs {
-      border-radius: 5px;
-      display: inline;
-      padding: 0 4px;
-
-
-    }
-
-    @media screen and (min-width: 750px) {
-      padding: 0 10%;
     }
   }
+
+  .pub-date {
+    display: block;
+    margin-bottom: 24px;
+
+    @media screen and (min-width: 750px) {
+      margin-bottom: 48px;
+    }
+  }
+
+  .hljs-keyword {
+    border-radius: 5px;
+    display: inline-flex;
+    padding: 2px 4px;
+  }
+
+  p > code.hljs {
+    border-radius: 5px;
+    display: inline;
+    padding: 0 4px;
+  }
+
+  @media screen and (min-width: 750px) {
+    padding-right: 10%;
+  }
+}
 </style>
