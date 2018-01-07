be463db58d64d1b05a34706df23f404837b2ad95
Ryan Will <ryanwilldev@gmail.com>
2018-01-07 12:43:43 -0600

    Formatting and color variables

--- components/NavLinks.vue
+++ components/NavLinks.vue
@@ -18,48 +18,50 @@ export default {
     </ul>
 </template>
 
-<style scoped type="text/css">
-  ul {
-    display: flex;
-    flex-direction: column;
-    height: 40%;
-    justify-content: space-around;
-    list-style: none;
-    margin: 0;
-    padding: 30px 0 0;
-  }
+<style scoped lang="scss">
+@import '../scss/variables';
 
+ul {
+  display: flex;
+  flex-direction: column;
+  height: 40%;
+  justify-content: space-around;
+  list-style: none;
+  margin: 0;
+  padding: 30px 0 0;
+}
+
+a {
+  font-size: 1.75em;
+  font-weight: normal;
+  margin: 0 10px 0;
+  padding: 5px 5px 0 5px;
+}
+
+a.router-link-active,
+a:hover {
+  border-bottom: 2px solid $blueAccent;
+}
+
+@media screen and (max-width: 1200px) {
   a {
-    font-size: 1.75em;
-    font-weight: normal;
-    margin: 0 10px 0;
-    padding: 5px 5px 0 5px;
+    font-size: 1.5em;
   }
+}
 
-
-  a.router-link-active, a:hover {
-    border-bottom: 2px solid #42b4d6;
+@media screen and (max-width: 1100px) {
+  ul {
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 0 10px 0 0;
+    width: 100%;
   }
+}
 
-  @media screen and (max-width: 1200px) {
-    a {
-      font-size: 1.5em;
-    }
+@media screen and (max-width: 600px) {
+  a {
+    font-size: 1.2em;
   }
-
-  @media screen and (max-width: 1100px) {
-    ul {
-      align-items: center;
-      flex-direction: row;
-      justify-content: space-around;
-      padding: 0 10px 0 0;
-      width: 100%;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    a {
-      font-size: 1.2em;
-    }
-  }
+}
 </style>
