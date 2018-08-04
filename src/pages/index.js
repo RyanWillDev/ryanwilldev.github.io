d416ca5d3a7e61b336dd1c6da20540aff697000c
Ryan Will <ryanwilldev@gmail.com>
2018-08-04 12:50:39 -0500

    remove alternating text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,6 @@ import Link from 'gatsby-link';
 import { PostList } from '../components/PostList';
 
 class IndexPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      words: ['Dev', 'Write', 'Learn', 'Build', 'Teach'],
-      index: 0,
-    };
-
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        index: this.state.index == 4 ? 0 : this.state.index + 1,
-      });
-    }, 2000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
   render() {
     const { edges: posts } = this.props.data.allMarkdownRemark;
     return (
@@ -36,9 +14,7 @@ class IndexPage extends Component {
             textAlign: 'center',
           }}
         >
-          RyanWill<span className="callout">
-            {this.state.words[this.state.index]}
-          </span>
+          RyanWill<span className="callout">Dev</span>
         </h1>
         <p style={{ fontSize: '1.25rem' }}>
           {this.props.data.site.siteMetadata.description}
